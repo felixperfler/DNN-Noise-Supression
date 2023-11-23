9a4f55cb9dfc277c3381dd182631ccdc977edf59
Felix Perfler <me@felixperfler.com>
2023-11-23 09:41:58 +0000

    switched files to src folder

--- conv_tasnet.py
+++ conv_tasnet.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+from src import sdr
 
-from utility import models, sdr
+from src import models
 
 
 # Conv-TasNet
